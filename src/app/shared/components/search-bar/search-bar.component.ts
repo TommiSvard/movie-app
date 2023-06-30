d743556ade49798814fc20f4cbe709c372c414c0
Tommi Svärd <tommi.svard@consid.se>
2023-06-30 15:49:17 +0200

    feat: add search logic

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'search-bar',
@@ -6,9 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
-  constructor() { }
+  public searchbarForm = this.fb.group({
+    searchbar: ['']
+  });
+  constructor(private fb: FormBuilder, private route: Router) { }
 
   ngOnInit(): void {
     
+  }
+
+  onSubmit(): void {
+    this.route.navigate(['/actor']);
+    console.log(this.searchbarForm.value);
   }
 }
