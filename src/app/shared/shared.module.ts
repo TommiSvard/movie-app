0492c46bace2077f3af144cf5512169a9dae4bc6
Tommi Svärd <tommi.svard@consid.se>
2023-06-30 15:50:11 +0200

    feat: add reactive forms module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
-
-
 import { NgModule } from "@angular/core";
 import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [SearchBarComponent, HeaderComponent],
-  exports: [SearchBarComponent, HeaderComponent]
+  exports: [SearchBarComponent, HeaderComponent],
+    imports: [ReactiveFormsModule],
 })
 export class SharedModule { }
