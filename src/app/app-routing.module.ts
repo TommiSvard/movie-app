beba470455b9d71a7e782113d82de8439aa12d69
Tommi Svärd <tommi.svard@consid.se>
2023-06-30 15:48:18 +0200

    feat: add actor route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'movie-search', loadChildren: () => import('./pages/movie-search/movie-search.module').then(m => m.MovieSearchModule) },
+  { path: 'actor', loadChildren: () => import('./pages/actor/actor.module').then(m => m.ActorModule) },
 ];
 
 @NgModule({
