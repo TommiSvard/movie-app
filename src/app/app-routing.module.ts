0136e6da0a5ee04e2348975612a05cff6778c9cc
Tommi Svärd <tommi.svard@consid.se>
2023-06-30 13:03:57 +0200

    feat: add movie-search route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movie-search', loadChildren: () => import('./pages/movie-search/movie-search.module').then(m => m.MovieSearchModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
